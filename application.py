09ca37817a75f9e75c1a300070e4f83b4e65e9a0
e2bias <70471970+e2bias@users.noreply.github.com>
2021-02-22 13:53:05 +0100

    Initial python code

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Example Page</h1>"
+    return "<h1>Hello ! Example Page</h1>"
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
